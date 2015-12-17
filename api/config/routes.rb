0a1c9bf7001c9a0055c79ec0c127793867a069de
webycel <anthony.web91@gmail.com>
2015-12-17 11:31:09 +0000

    Set the routes contraints for the api

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,9 @@
 TardyApi::Application.routes.draw do
   devise_for :users
 
+  # Api definition
+  namespace :api, defaults: { format: :json}, constraints: { subdomain: 'api' }, path: '/' do
+
+  end
 
 end
