5b52809bf8d9e27f0465fd1c116e8fbe5f4dbe48
webycel <anthony.web91@gmail.com>
2015-12-17 17:27:23 +0000

    authenticate with token method to handle access to actions

--- api/app/controllers/concerns/authenticable.rb
+++ api/app/controllers/concerns/authenticable.rb
@@ -5,4 +5,8 @@ module Authenticable
 		@current_user ||= User.find_by(auth_token: request.headers['Authorization'])
 	end
 
+	def authenticate_with_token!
+		render json: { errors: "Not authenticated", status: :unauthorized unless current_user.present? }
+	end
+
 end
