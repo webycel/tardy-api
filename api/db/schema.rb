e2c6d6c1d1218fa73fe354b4b62126beea1e5c9f
webycel <anthony.web91@gmail.com>
2015-12-17 14:58:20 +0000

    added user auth token

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217114951) do
+ActiveRecord::Schema.define(version: 20151217145411) do
 
   create_table "teams", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 20151217114951) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "auth_token"
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
