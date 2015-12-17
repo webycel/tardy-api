1b723f8792ccc9f23b0acfab12a8395c2c54081b
webycel <anthony.web91@gmail.com>
2015-12-17 17:14:34 +0000

    session controller create action

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,6 +5,7 @@ Api::Application.routes.draw do
   namespace :api, defaults: { format: :json} do
     scope module: :v1 do
       resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 
