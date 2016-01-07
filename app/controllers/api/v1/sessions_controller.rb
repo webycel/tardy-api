d2dc296dd0dbd69a1a858922cd2834528c5719f4
webycel <anthony.web91@gmail.com>
2016-01-07 11:49:57 +0000

    removed commented code

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,9 +8,9 @@ class Api::V1::SessionsController < ApplicationController
 		user = user_email.present? && User.find_by(email: user_email)
 
 		if user && user.valid_password?(user_password)
-			# sign_in user, store: false
-			# user.generate_authentication_token!
-			# user.save
+			sign_in user, store: false
+			user.generate_authentication_token!
+			user.save
 			render json: user, status: 200, location: [:api, user]
 		else
 			render json: { errors: "Invalid email or password" }, status: 422
